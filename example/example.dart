c1be88656a54c9c36a4c9b145ad0c06b102a88ea
Yovanny Nogales <nverayovanny@gmail.com>
2020-05-16 19:22:48 -0700

    #time 3h #comment The grouped frecuency functions to get a grouped table data was added

--- example/example.dart
+++ example/example.dart
@@ -1,7 +1,7 @@
 import '../lib/src/stats.dart';
 
 void main() {
-  Stats stats = Stats([1,1,1,2,3,4,2,5,3]);
+  Stats stats = Stats([22, 19, 16, 13, 18, 15, 20, 14, 15, 16, 15, 16, 20, 13, 15, 18, 15, 13, 18, 16]);
 
   print(stats.sortedList);
   print(stats.min);
@@ -12,8 +12,22 @@ void main() {
   print(stats.getMode());
   print(stats.getMedian());
 
-  print("-- Disperions measure --");
+  print('-- Disperions measure --');
   print(stats.getVariance());
   print(stats.getStandardDeviation());
   print(stats.getRange());
+
+  print('-- Grouped frecuency table --\n\n');
+  Map<String, dynamic> groupedClassLimit;
+  List<Map<String, dynamic>> groupedTable = stats.getGroupedFrecuencyMap();
+  for (groupedClassLimit in groupedTable) {
+    print('------------------');
+    print('  Lower class limit: ${ groupedClassLimit['lowerClassLimit'] }');
+    print('  Upper class limit: ${ groupedClassLimit['upperClassLimit'] }');
+    print('  Mid point: ${ groupedClassLimit['midPoint'] }');
+    print('  Absolute frecuency: ${ groupedClassLimit['absolueteFrecuency'] }');
+    print('  Accumulated Relative frecuency: ${ groupedClassLimit['accumulatedRelativeFrecuency'] }');
+    print('  Accumulated frecuency: ${ groupedClassLimit['accumulatedFrecuency'] }');
+    print('------------------\n\n');
+  }
 }
